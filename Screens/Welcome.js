6369d7c86138fdc140cda5c84c57c7d3d1e03bbe
juanrivarola <jeryflr@gmail.com>
2022-09-26 18:00:50 -0300

    creacion

---
+++ Screens/Welcome.js
@@ -0,0 +1,70 @@
+import { StatusBar } from 'expo-status-bar';
+import { ScrollView, StyleSheet, Text, View, Image, FlatList, Button, Alert } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function Welcome() {
+  const cities={
+    name:"tokyo",
+    name:"buenos aires"
+  }
+  return (
+    <View style={styles.container}>
+      <View style={styles.title}>
+         <Text style={styles.title.my}>My</Text>
+         <Text style={styles.title.tin}>Tinerary</Text>
+      </View>
+      <ScrollView>
+      <Text style={styles.text2}>Take the trip of you always wanted, to the city of your drems</Text>
+      <View style={styles.calltoaction}>
+        <Button title="Let's go" color={'#B465FF'} onPress={() => Alert.alert('KE APRETAS')}></Button>
+      <Image style={styles.img} source={require('../assets/img/arrow.png')} />
+      <Text style={styles.text2}>Tap here to see the best destinys</Text>
+      </View>
+      </ScrollView>
+      <FlatList data={cities}
+                 numColumns={2}
+                 keyExtractor={(city)=>city.name}></FlatList>
+      <StatusBar style="light" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  title:{
+     flex:0,
+     flexDirection:'row',
+    my:{
+        fontSize:90,
+        color:'rgb(244, 89, 0)'
+    },
+    tin:{
+        fontSize:90,
+        color:'brown',
+    }
+  },
+  text:{
+    fontSize:70,
+    color: '#fff',
+    marginBottom:30
+  },
+  text2:{
+    fontSize:30,
+    color: '#fff',
+    textAlign:'center',
+    marginBottom:30,
+  },
+  img:{
+   width: 90,
+   height:140,
+  },
+  calltoaction:{
+    flex:1,
+    alignItems:'center',
+  },
+});
