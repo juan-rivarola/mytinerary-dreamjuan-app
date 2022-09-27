5625bb6f377498ad532677afc544346bb823845d
juanrivarola <jeryflr@gmail.com>
2022-09-27 13:03:45 -0300

    redux

--- App.js
+++ App.js
@@ -4,11 +4,15 @@ import Welcome from './Screens/Welcome';
 import React from 'react'
 import {NavigationContainer} from '@react-navigation/native'
 import Drawer from './navigation/Drawer';
+import { Provider } from "react-redux";
+import { store } from './features/store';
 
 export default function App() {
   return (
     <NavigationContainer>
+      <Provider store={store}>
       <Drawer />
+      </Provider>
     </NavigationContainer>
   )
 }
