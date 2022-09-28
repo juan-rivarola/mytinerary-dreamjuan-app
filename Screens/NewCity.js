17d6fb39f0cfcf25392ed13d409b5e10ab9370d8
Elvio Galeano <elvio.galeano827@gmail.com>
2022-09-28 12:27:20 -0300

    se agrego header y footer

--- Screens/NewCity.js
+++ Screens/NewCity.js
@@ -1,10 +1,12 @@
 import { View, Text,StyleSheet, Button } from 'react-native'
 import React, { useRef, useState } from 'react'
-import { TextInput } from 'react-native-gesture-handler'
+import { ScrollView, TextInput } from 'react-native-gesture-handler'
 import { useCreateCityMutation } from '../features/citiesApi'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export default function NewCity() {
-
+    const scrollRef = useRef()
     const [city,setCity] =useState('')
     const [country,setCountry] =useState('')
     const [image,setImage] =useState('')
@@ -26,17 +28,20 @@ export default function NewCity() {
 
   return (
     <View style={styles.container}>
+        <ScrollView ref={scrollRef}>
         <View style={styles.newCity}>
+            <Header />
             <Text style={styles.title}>New City</Text>
-
                 <TextInput  placeholder="City" style={styles.input} onChangeText={newT=>setCity(newT)}/>
                 <TextInput  placeholder="Country" style={styles.input} onChangeText={newT=>setCountry(newT)}/>
                 <TextInput  placeholder="Image" style={styles.input} onChangeText={newT=>setImage(newT)}/>
                 <TextInput  placeholder="Population" style={styles.input} onChangeText={newT=>setPopulation(newT)}/>
                 <TextInput  placeholder="Fundation" style={styles.input} onChangeText={newT=>setFundation(newT)}/>
-
             <Button title='Add city' onPress={create}></Button>
+            <Footer />
         </View>
+        <Button style={styles.goTop} title="Top" onPress={()=> scrollRef.current.scrollTo({ x: 0, y: 0, animated: true })}/>
+        </ScrollView>
     </View>
   )
 }
