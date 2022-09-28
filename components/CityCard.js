efe7502367fe045a6d100e62d3e39e28a628d788
Elvio Galeano <elvio.galeano827@gmail.com>
2022-09-28 19:14:37 -0300

    se agrego la props navigation

--- components/CityCard.js
+++ components/CityCard.js
@@ -2,12 +2,12 @@ import React from "react";
 import { View, StyleSheet, Image,Text, Pressable, Alert } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-export default function(props){
+export default function CityCard(props){
     const godetails= ({navigation}) =>navigation.navigate('Details')
     const allCard = props.data.response ? props.data.response : props.data
     const card =(item) =>(
         <View style={styles.cards} key={item._id}>
-            <Pressable onPressIn={()=>Alert.alert('Detalles de '+item.name)}>
+            <Pressable onPress={()=>Alert.alert('Detalles de '+item.name)}>
             <Image source={{uri:item.image}} style={styles.img}/>
             <Text style={styles.country}>{item.name}</Text>
             </Pressable>
