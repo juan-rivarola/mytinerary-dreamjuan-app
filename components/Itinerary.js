4ce408d0ec0657fb4b191c2de9a6a94727c0048f
juanrivarola <jeryflr@gmail.com>
2022-09-29 11:48:03 -0300

    render act

--- components/Itinerary.js
+++ components/Itinerary.js
@@ -2,6 +2,7 @@ import { View, Text,StyleSheet,ScrollView, Button } from 'react-native'
 import React, { useRef, useState } from 'react'
 import { useBycityQuery, useByuserQuery } from '../features/itinerariesApi'
 import Footer from './Footer'
+import Activities from './Activities'
 
 export default function Itinerary(props) {
     let idCity = "630e518bbe28a5faae2423c0"
@@ -9,17 +10,18 @@ export default function Itinerary(props) {
 
     let {data:itineraries} = useBycityQuery(idCity)
     itineraries= itineraries
-// console.log(id)
+ //console.log(itineraries)
   return (
                     <View style={styles.container}>
                         <View style={styles.container.itinerary}>
                             {itineraries.map(itinerary =>(
-                                <View style={styles.itineraryBox}>
+                                <View style={styles.itineraryBox} key={itinerary.name}>
                                     <Text style={styles.description.name}>{itinerary.name}</Text>
                                     <Text style={styles.description}>Price: {itinerary.price}</Text>
                                     <Text style={styles.description}>Duration: {itinerary.duration}</Text>
                                     <Text style={styles.description}> {itinerary.tags}</Text>
                                     <Text style={styles.description}>Likes: {itinerary.likes.length}</Text>
+                                    <Activities ItineraryId={itinerary._id}/>
                                 </View>
                             ))}
                         </View>
