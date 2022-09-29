a0a5ebf694415ec51eb85903c6272e74a5ce6049
juanrivarola <jeryflr@gmail.com>
2022-09-29 13:51:34 -0300

    fix

--- navigation/Drawer.js
+++ navigation/Drawer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {createDrawerNavigator} from '@react-navigation/drawer'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Welcome from '../Screens/Welcome'
 import Cities from '../Screens/Cities'
 import User from '../Screens/User'
@@ -7,18 +8,20 @@ import NewCity from '../Screens/NewCity'
 import Details from '../Screens/Details'
 import Itinerary from '../components/Itinerary'
 import Comments from '../components/Comments'
+import MyStackScreen from './Stack';
 
 const DrawerNav = createDrawerNavigator()
 
 export default function Drawer(){
-    return(
+    return(<>
         <DrawerNav.Navigator>
             <DrawerNav.Screen name='Home' component={Welcome} />
             <DrawerNav.Screen name='Cities' component={Cities}/>
             <DrawerNav.Screen name='New City' component={NewCity}/>
             <DrawerNav.Screen name='Profile' component={User}/>
-            <DrawerNav.Screen name='Details' component={Details}/>
             <DrawerNav.Screen name='Comments' component={Comments}/>
+            <DrawerNav.Screen name=' ' component={Details} />
         </DrawerNav.Navigator>
+    </>
     )
 }
